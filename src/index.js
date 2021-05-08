aecfe6fbc7c2d6f2d86be199c4d9031b67461f30
devJang <devjang2016@gmail.com>
2021-05-08 23:05:45 +0900

    refactor: use thunk

--- src/index.js
+++ src/index.js
@@ -1,16 +1,58 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import ReduxThunk from 'redux-thunk';
 
-const INCREMENT = '@action/increment';
-const DECREMENT = '@action/decrement';
-const RESET = '@action/reset';
+/**
+ * @see {@link redux-thunk}
+ * @see {@link https://github.com/reduxjs/redux-thunk/blob/master/src/index.js}
+ */
+function createThunkMiddleware(extraArgument) {
+	return ({ dispatch, getState }) => (next) => (action) => {
+		if (typeof action === 'function') {
+			return action(dispatch, getState, extraArgument);
+		}
+
+		return next(action);
+	};
+}
+
+const ReduxThunk = createThunkMiddleware();
 
 // 초기 상태
 const initState = {
 	count: 0,
 	message: '안녕하세요!!!!',
 	imgUrl: '',
+};
+
+// Action Type
+const INCREMENT = '@action/increment';
+const DECREMENT = '@action/decrement';
+const RESET = '@action/reset';
+
+// Action
+const increment = {
+	type: INCREMENT,
+	payload: '증가하겠습니다',
+};
+
+const decrement = {
+	type: DECREMENT,
+	message: '감소하겠습니다',
+};
+
+const reset = {
+	type: RESET,
+};
+
+const getDog = () => (dispatch, getState) => {
+	fetch('https://dog.ceo/api/breeds/image/random/' + getState().count)
+		.then((response) => response.json())
+		.then(({ message, status }) => {
+			const [firstDogImageUrl] = message;
+
+			dispatch({ type: 'GET_DOG_SUCCESS', payload: firstDogImageUrl });
+		})
+		.catch((e) => dispatch({ type: 'GET_DOG_ERROR', error: e }));
 };
 
 // Reducer
@@ -52,7 +94,7 @@ function reducer(state = initState, action) {
 const beholder = (store) => (next) => (action) => {
 	setTimeout(() => {
 		if (action.type === RESET) {
-			next({
+			return next({
 				type: RESET,
 				message: '이제 아무것도 안하는건가요?',
 			});
@@ -67,7 +109,7 @@ const render = function (state) {
 
 	document.querySelector('.count').innerHTML = `<h1>${count}</h1>`;
 	document.querySelector('.message').innerHTML = `<h1>${message}${count === 0 ? '빨리 뭐라도 해볼까요?' : ''}</h1>`;
-	document.querySelector('.dog-album').innerHTML = `<img src="${imgUrl}">`;
+	document.querySelector('.dog-album').innerHTML = `<img src="${imgUrl}" style="max-height: 500px;">`;
 };
 
 // Store
@@ -84,32 +126,17 @@ const init = function () {
 	render(initState);
 
 	document.querySelector('.increment-btn').addEventListener('click', function () {
-		store.dispatch({
-			type: INCREMENT,
-			payload: '증가하겠습니다',
-		});
-
-		fetch('https://dog.ceo/api/breeds/image/random/' + store.getState().count)
-			.then((response) => response.json())
-			.then(({ message, status }) => {
-				const [firstDogImageUrl] = message;
-
-				store.dispatch({ type: 'GET_DOG_SUCCESS', payload: firstDogImageUrl });
-			})
-			.catch((e) => store.dispatch({ type: 'GET_DOG_ERROR', error: e }));
+		store.dispatch(increment);
+		store.dispatch(getDog());
 	});
 
 	document.querySelector('.decrement-btn').addEventListener('click', function () {
-		store.dispatch({
-			type: DECREMENT,
-			message: '감소하겠습니다',
-		});
+		store.dispatch(decrement);
+		store.dispatch(getDog());
 	});
 
 	document.querySelector('.reset-btn').addEventListener('click', function () {
-		store.dispatch({
-			type: RESET,
-		});
+		store.dispatch(reset);
 	});
 };
 
